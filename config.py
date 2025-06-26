65b26d140d362c05a1e33afbaad51e4a7ccb6e06
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2025-06-26 12:26:52 -0500

    Delete backend files from main directory (after moving to backend directory)

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    with open('static/secret.txt') as fp:
-        SECRET_KEY = fp.readline()
-        SQLALCHEMY_DATABASE_URI = fp.readline()
