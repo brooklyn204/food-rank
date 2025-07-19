668b5472cf1a93a815226980c6194364bda5ef3a
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2025-07-19 13:44:09 -0500

    Allow users to enter their names as a part of voting

--- frontend/app/api/vote/route.jsx
+++ frontend/app/api/vote/route.jsx
@@ -2,9 +2,11 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   const body = await request.json();
-  const votes = body.votes;
+  const code = body.code;
+  const name = body.name;
+  const votes = body.order;
 
-  console.log('Saving votes:', votes); // or save to DB, etc.
+  console.log('Saving votes:', votes, 'for', name); // or save to DB, etc.
 
   return NextResponse.json({ message: `Saving ${votes}` });
 }
