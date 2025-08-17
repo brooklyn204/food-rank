574791e3ab9136ed17f40e142f17c93a41bfacfc
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2025-08-16 21:35:39 -0500

    Updated results route to get from database, computing location scores dynamically from voters' votes

--- frontend/app/api/results/route.jsx
+++ frontend/app/api/results/route.jsx
@@ -1,6 +1,35 @@
 import { NextResponse } from 'next/server';
-import Location from '../../../lib/models/Location';
-import Voter from '../../../lib/models/Voter';
+import { getContainer } from '../../../lib/cosmosClient';
+
+async function countVotes(voters, numLocations) {
+  const votes = new Array(numLocations).fill(0);
+  // Compute each voter's vote for each location and add to total location votes
+  for (let i=0; i<voters.length; i++) {
+    const voter = voters[i];
+    const order = voter.orderedLocations;
+    const lineLocation = voter.lineLocation;
+
+    // Compute location votes based on the order (weighted borda count algorithm)
+    const bonus = (order.length - lineLocation) + 1; // Preferences are taken more seriously for picky voters, but even voters who put all above the line get a little bonus (hardcoded 1 is questionable.. TODO: change to ~20% of total list length??) -- TODO: user approval testing (alternate idea: distance from line if above the line, else 0)
+    for (let i=0; i<order.length; i++) {
+      const location = order[i];
+      const score = order.length - 1 - i; // Earlier in the list = higher preference = higher score
+      let vote = 0;
+      if (location >= 0 && location < order.length) {
+        if (i < lineLocation) {
+          // Location score = preference level in list + bonus for being above the line
+          vote = bonus + score; 
+        } else {
+          // Location score = just preference level in list (no bonus)
+          vote += score; 
+        }
+      } // TODO: else case with error handling
+      console.log(`User ${voter.name} voted ${vote} for location ${location}`);
+      votes[location] += vote;
+    }
+  }
+  return votes;
+}
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
@@ -9,32 +38,39 @@ export async function GET(request) {
 
   console.log('Getting results for:', code);
 
-  const name = "My Group";
-  const locations = [
-      new Location('McDonalds', 'mcdonalds.com', 4),
-      new Location('Wendys', 'wendys.com', 7),
-      new Location('Chipotle', 'chipotle.com', 6),
-      new Location('Arbys', 'arbys.com', 7),
-      new Location('Shake Shack', 'shakeshack.com',4),
-    ];
-    const voterData = [
-      new Voter( 
-        "Emily",
-        [0,3,2,-1,4,1],
-        3),
-      new Voter( 
-      "Josh",
-      [4,-1,3,1,0,2],
-      1),
-      new Voter( 
-      "Alex",
-      [2,1,3,4,0,-1],
-      5),
-      new Voter( 
-      "Nancy",
-      [1,3,4,-1,0,2],
-      3)
-    ];
+  // Get group item from database
+  // TODO: abstract away item access
+  const container = await getContainer();
+  const query = {
+    query: 'SELECT TOP 1 * FROM c WHERE c.groupCode = @code',
+    parameters: [{ name: '@code', value: code }],
+  };
+  const { resources: items } = await container.items.query(query).fetchAll();
+  const item = items.length > 0 ? items[0] : null;
+  // TODO: error handling if no items found (item is null)
+  const name = item.name || "Defaults"; // TODO: error handling if no name
+  const locations = item.locations || []; // TODO: error handling if no locations
+  const voters = item.voters || []; // TODO: error handling if no voters
+  
 
-  return NextResponse.json({ message: `Results for ${code}`, name: name, locations: locations, voters: voterData });
+  // Ensure the field exists and is a list
+  if (!Array.isArray(voters) ) {
+    // TODO: better error handling
+    throw new Error(`Field ${votersField} is not an array`);
+  }
+  
+  const votes = await countVotes(voters, locations.length);
+
+  // Update each location's votes
+  for (let i=0; i<locations.length; i++) {
+    if (i<votes.length) {
+      locations[i].votes = votes[i];
+    } else {
+      locations[i].votes = 0;
+    }
+  }
+
+  console.log(`Computed total votes: ${locations.map(loc => loc.votes)}`);
+
+  return NextResponse.json({ message: `Results for ${code}`, name: name, locations: locations, voters: voters });
 }
--- frontend/app/api/vote/route.jsx
+++ frontend/app/api/vote/route.jsx
@@ -15,28 +15,6 @@ export async function POST(request) {
   order.splice(lineLocation, 1);
   // TODO: EXTENSIVE error handling (check if lineLocation is -1 (before splicing), check if all items in orderedLocations are valid, etc.)
 
-  // Create voter object
-  const voter = new Voter(name, order, lineLocation);
-
-  // Compute location votes based on the order (weighted borda count algorithm)
-  const bonus = (order.length - lineLocation) + 1; // Preferences are taken more seriously for picky voters, but even voters who put all above the line get a little bonus (hardcoded 1 is questionable.. TODO: change to ~20% of total list length??) -- TODO: user approval testing (alternate idea: distance from line if above the line, else 0)
-  const votes = new Array(order.length).fill(0);
-  for (let i=0; i<order.length; i++) {
-    const location = order[i];
-    const score = order.length - 1 - i; // Earlier in the list = higher preference = higher score
-    if (location >= 0 && location < order.length) {
-      if (i < lineLocation) {
-        // Location score = preference level in list + bonus for being above the line
-        votes[location] = bonus + score; 
-      } else {
-        // Location score = just preference level in list (no bonus)
-        votes[location] = score; 
-      }
-    } // TODO: else case with error handling
-  }
-
-  console.log(`Computed votes: ${votes} for user ${name}`);
-
   // Get group item from database
   // TODO: abstract away item access
   const container = await getContainer();
@@ -49,12 +27,14 @@ export async function POST(request) {
   const partitionKey = item.id;
   // TODO: error handling if no items found (item is null)
 
+  // Create voter object
+  const voter = new Voter(name, order, lineLocation);
+
   // Save user's votes
   // Ensure the field exists and is a list
-  const votersField = "voters";
-  if (!Array.isArray(item[votersField]) ) {
-    if (item[votersField] === undefined) {
-    item[votersField] = [];
+  if (!Array.isArray(item.voters) ) {
+    if (item.voters === undefined) {
+    item.voters = [];
     } else {
       // TODO: better error handling
       throw new Error(`Field ${votersField} exists but is not an array`);
@@ -63,15 +43,7 @@ export async function POST(request) {
 
   // Add the voter to the array in the database item
   console.log(`Adding voter ${JSON.stringify(voter)} to item ${JSON.stringify(item)}\n`);
-  item[votersField].push(voter);
-
-  // Update each location's votes
-  for (let i=0; i<item.locations.length && i<votes.length; i++) {
-    if (typeof item.locations[i].votes !== 'number') { // TODO: change to const variable for type string
-      item.locations[i].votes = 0; // Initialize if not present or not a number
-    }
-    item.locations[i].votes += votes[i];
-  }
+  item.voters.push(voter);
 
   // Replace the document
   const { resource: updatedItem } = await container
@@ -80,5 +52,5 @@ export async function POST(request) {
 
   console.log(`Saved item ${JSON.stringify(item)}\n`);
   
-  return NextResponse.json({ message: `Saving ${votes}` });
+  return NextResponse.json({ message: `Saving ${voter}` });
 }
